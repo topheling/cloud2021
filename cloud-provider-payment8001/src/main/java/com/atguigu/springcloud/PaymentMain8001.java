b26832fdc6a6d936eea38a289b5d6c068fc6a3b6
heling <www_heling@sina.com>
2021-05-06 18:21:30 +0800

    初次提交

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  8001启动成功   ლ(´ڡ`ლ)ﾞ ");
+    }
+}
---
+++ md/SpringCloud.md
@@ -0,0 +1,17 @@
+### 1、Cloud升级
+
+![image-20210506113922181](C:\Users\LL\AppData\Roaming\Typora\typora-user-images\image-20210506113922181.png)
+
+开发时：约定>配置>编码
+
+![image-20210506114130600](C:\Users\LL\AppData\Roaming\Typora\typora-user-images\image-20210506114130600.png)
+
+
+
+### 1、微服务模块
+
+1. 建module
+2. 改pom
+3. 写yml
+4. 主启动
+5. 业务类
