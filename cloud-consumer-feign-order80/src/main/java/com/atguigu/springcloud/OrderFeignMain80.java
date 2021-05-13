3ea9691865d21798703fdfa3f1d4f62a16def9b2
heling <www_heling@sina.com>
2021-05-13 16:39:38 +0800

    OpenFeign

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  80启动成功   ლ(´ڡ`ლ)ﾞ ");
+    }
+}
