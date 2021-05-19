51f1177bb281d9891466ed15b57e639e67bfc170
贺岭 <www_heling@sina.com>
2021-05-19 15:56:26 +0800

    Ribbon替换默认的负载均衡规则

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  3344启动成功   ლ(´ڡ`ლ)ﾞ ");
+    }
+}
