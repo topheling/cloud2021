a4b2be6626cd54778a277b24ee5a2843d4ec838b
heling <www_heling@sina.com>
2021-05-25 17:51:09 +0800

    Nacos

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/spring/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.atguigu.spring.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //支持Nacos动态的刷新功能
+@Slf4j
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
