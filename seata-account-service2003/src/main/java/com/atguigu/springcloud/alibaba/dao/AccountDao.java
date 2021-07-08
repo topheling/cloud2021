cd4742bc8ab49e144477e02ef01a0e13ad94f92d
heling <www_heling@sina.com>
2021-07-08 11:43:30 +0800

    订单module、 库存module、 账户module

---
+++ seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/dao/AccountDao.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.alibaba.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.math.BigDecimal;
+
+
+/**
+ * @auther zzyy
+ * @create 2019-12-11 16:46
+ */
+@Mapper
+public interface AccountDao {
+
+    /**
+     * 扣减账户余额
+     */
+    void decrease(@Param("userId") Long userId, @Param("money") BigDecimal money);
+
+}
+
+
+
